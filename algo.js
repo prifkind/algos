2a8f1f419cbca74c86ba3d9354bad5caa214de28
Peter Rifkind <prifkind@gmail.com>
2023-04-14 09:36:32 -0400

    Daily algo

--- algo.js
+++ algo.js
@@ -1,23 +1,12 @@
-var longestPalindrome = function (s) {
-    let res = '';
-    for(let i = 0; i < s.length; i++){
-        // Odd - if there is only 1 char in the middle, like 'bob'
-        let lOdd = i, rOdd = i;
-        expandAroundCenter(lOdd, rOdd)
-        //Even - 2 chars in the middle, like 'kbbk'
-        let lEven = i, rEven = i + 1;
-        expandAroundCenter(lEven, rEven);
-    }
-  // helper
-    function expandAroundCenter(left, right){
-      while(left >= 0 && right < s.length && s[left] === s[right]){
-        let subStr = s.substring(left, right + 1);
-        if(subStr.length > res.length){
-          res = subStr;
-        }
-        left--;
-        right++;
-      }
-    }
-    return res;
-};
+var twoSum = function(nums, target) {
+	let hash = {};
+
+	for(let i = 0; i < nums.length; i++) {
+		const n = nums[i];
+		if(hash[target - n] !== undefined) {
+			return [hash[target - n], i];
+		}
+		hash[n] = i;
+	}
+	return [];
+}
