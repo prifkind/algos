104353ae4ea5c81306c765981936f855d77d35ac
Peter Rifkind <prifkind@gmail.com>
2023-04-17 12:44:01 -0400

    Daily algo

--- algo.js
+++ algo.js
@@ -1,34 +1,15 @@
-var maxArea = function (height) {
-	// Use two pointers at the beginning and end of the array
+var twoSum = function (numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
 
-	let left = 0;
-	let right = height.length - 1
-	let maximum = 0;
+    while (left < right) {
+        if (numbers[left] + numbers[right] > target) {
+            right--
+        } else if (numbers[left] + numbers[right] < target) {
+            left++
+        } else if (numbers[left] + numbers[right] === target) {
+            return [left + 1, right + 1]
+        }
+    }
 
-	// Loop through the array
-	while (left < right) {
-	  let area;
-
-	  // Check which height is lower (left or right), and calculate the area based on the lower height
-	  if (height[left] < height[right]) {
-		area = height[left] * (right - left)
-		// Slide the smaller of the two sides
-		left++
-	  } else {
-		area = height[right] * (right - left)
-		// Slide the smaller of the two sides
-		right--
-	  }
-
-	  // Compare that to any previous value for maximum height.  If the current area exceeds the previous maximum, re-assign it to be the new maximum.
-	  if (area > maximum) {
-		maximum = area
-	  }
-
-
-	}
-
-	return maximum
-
-
-  };
+};
