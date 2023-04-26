c8ff9fadfb6c1ca27650a3a3699b283b28f3c695
Peter Rifkind <prifkind@gmail.com>
2023-04-26 08:56:48 -0400

    Daily algo

--- algo.js
+++ algo.js
@@ -1,22 +1,25 @@
-var removeNthFromEnd = function(head, n) {
-    let dummy = new ListNode(0);
-    dummy.next = head;
-    let first = dummy;
-    let second = dummy;
-
-    // Move first pointer n nodes ahead of second pointer
-    for(let i = 0; i <= n; i++){
-        first = first.next;
+var isValid = function (s) {
+    // Parse string to array
+    let splitString = s.split('')
+    let stack = []
+    let dictionary = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    }
+    if (splitString.length === 1) return false
+    // Loop through array.  Check count of each character and update values for ordering
+    for (let i = 0; i < splitString.length; i++) {
+        // If it's an opening character, push it to the end of the stack
+        if (splitString[i] === '{' || splitString[i] === '[' || splitString[i] === '(') {
+            stack.push(splitString[i])
+        }
+        // If it's a closing character, compare the last character on the stack to the dictionary and see if it matches the closing character
+        else if (dictionary[stack[stack.length - 1]] === splitString[i]) {
+            // Pop the character off the stack if it matches
+            stack.pop()
+        } else return false
     }
 
-    // Move both pointers until first pointer reaches the end of the list
-    while(first !== null){
-        second = second.next;
-        first = first.next;
-    }
-
-    // Remove the nth node from the end of the list
-    second.next = second.next.next;
-
-    return dummy.next;
+    if (stack.length === 0) { return true } else return false
 };
