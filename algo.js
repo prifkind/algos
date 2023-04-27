7bf0d76cec19a81324dc47ca11753a7a3d0b94db
Peter Rifkind <prifkind@gmail.com>
2023-04-27 10:09:04 -0400

    Daily algo

--- algo.js
+++ algo.js
@@ -1,25 +1,36 @@
-var isValid = function (s) {
-    // Parse string to array
-    let splitString = s.split('')
-    let stack = []
-    let dictionary = {
-        '(': ')',
-        '{': '}',
-        '[': ']'
-    }
-    if (splitString.length === 1) return false
-    // Loop through array.  Check count of each character and update values for ordering
-    for (let i = 0; i < splitString.length; i++) {
-        // If it's an opening character, push it to the end of the stack
-        if (splitString[i] === '{' || splitString[i] === '[' || splitString[i] === '(') {
-            stack.push(splitString[i])
-        }
-        // If it's a closing character, compare the last character on the stack to the dictionary and see if it matches the closing character
-        else if (dictionary[stack[stack.length - 1]] === splitString[i]) {
-            // Pop the character off the stack if it matches
-            stack.pop()
-        } else return false
-    }
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
 
-    if (stack.length === 0) { return true } else return false
+    const start = new ListNode(-Infinity)
+    let prev = start
+
+    // Note that list1 and list2 are both nodes
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            prev.next = list1
+            prev = list1
+            list1 = list1.next
+        } else {
+            prev.next = list2
+            prev = list2
+            list2 = list2.next
+        }
+
+    }
+        if (!list1) prev.next = list2
+        if (!list2) prev.next = list1
+
+    return start.next
+
 };
