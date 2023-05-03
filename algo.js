707bed86dc577c9d7ac86e263d4bad6612116a3a
Peter Rifkind <prifkind@gmail.com>
2023-05-03 08:22:20 -0400

    Daily algo

--- algo.js
+++ algo.js
@@ -1,47 +1,16 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
  */
-/**
- * @param {ListNode[]} lists
- * @return {ListNode}
- */
-function mergeLists(a, b) {
-    const dummy = new ListNode(0);
-    let temp = dummy;
-     while (a !== null && b !== null) {
-         if (a.val < b.val) {
-             temp.next = a;
-             a = a.next;
-         } else {
-             temp.next = b;
-             b = b.next;
-         }
-         temp = temp.next;
-     }
-    if (a !== null) {
-        temp.next = a;
-    }
-    if (b !== null) {
-        temp.next = b;
-    }
-    return dummy.next;
-}
+var search = function (nums, target) {
+    let counter = nums.length - 1
 
-var mergeKLists = function(lists) {
-    if (lists.length === 0 ) {
-        return null;
+    while (counter >= 0) {
+        if (nums.pop() === target) {
+            return counter
+        } else counter--
     }
 
-
-    while (lists.length > 1) {
-        let a = lists.shift();
-        let b = lists.shift();
-        const h = mergeLists(a, b);
-        lists.push(h);
-    }
-    return lists[0];
+    return -1
 };
