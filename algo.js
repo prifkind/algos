3447208918588200ee72ee557e7be232d0eef884
Peter Rifkind <prifkind@gmail.com>
2023-06-05 15:57:05 -0400

    Daily algo

--- algo.js
+++ algo.js
@@ -1,38 +1,16 @@
-var combinationSum = function(candidates, target) {
-
-    if (!candidates) {
-        return [];
-    }
-
-    // if (target === 0) {
-    //     return [[]];
-    // }
-
-
-    candidates.sort((a,b) => { return a - b});
-
-    let paths = [];
-
-
-    let find = function (t, p, i) {
-
-        if (t === 0) {
-            paths.push(p);
-            return;
-        } else if (t < 0) {
-            return;
-        } else {
-            while (i < candidates.length && t - candidates[i] >= 0) {
-                find(t - candidates[i], [...p, candidates[i]], i)
-                i++;
-            }
-
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    let rotate = 0;
+    for(row = 0; row < matrix.length/2; row++){
+        for(col = row; col < matrix.length - 1 - row; col++){
+            rotate = matrix[row][col];
+            matrix[row][col] = matrix[matrix.length - 1 - col][row];
+            matrix[matrix.length-1-col][row] = matrix[matrix.length-1-row][matrix.length-1-col];
+            matrix[matrix.length-1-row][matrix.length-1-col] = matrix[col][matrix.length-1-row];
+            matrix[col][matrix.length-1-row] = rotate;
         }
-
     }
-
-
-    find (target, [], 0);
-
-    return paths;
 };
