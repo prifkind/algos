83fb804982cacb27aef8e1eeeb311957e79bd61d
Peter Rifkind <prifkind@gmail.com>
2023-06-06 16:24:35 -0400

    Daily algo

--- algo.js
+++ algo.js
@@ -1,16 +1,12 @@
 /**
- * @param {number[][]} matrix
- * @return {void} Do not return anything, modify matrix in-place instead.
+ * @param {string[]} strs
+ * @return {string[][]}
  */
-var rotate = function(matrix) {
-    let rotate = 0;
-    for(row = 0; row < matrix.length/2; row++){
-        for(col = row; col < matrix.length - 1 - row; col++){
-            rotate = matrix[row][col];
-            matrix[row][col] = matrix[matrix.length - 1 - col][row];
-            matrix[matrix.length-1-col][row] = matrix[matrix.length-1-row][matrix.length-1-col];
-            matrix[matrix.length-1-row][matrix.length-1-col] = matrix[col][matrix.length-1-row];
-            matrix[col][matrix.length-1-row] = rotate;
-        }
+var groupAnagrams = function(strs) {
+    let obj = {};
+    for (let str of strs) {
+        let letters = str.split("").sort().join("");
+        obj[letters] ? obj[letters].push(str) : obj[letters] = [str];
     }
+    return Object.values(obj);
 };
