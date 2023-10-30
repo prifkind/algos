c25745d06bee6f5f2e21ecba96b1e763d5ec18c9
Peter Rifkind <prifkind@gmail.com>
2023-10-30 16:03:09 -0400

    Daily algo

--- algo.py
+++ algo.py
@@ -1,28 +1,46 @@
 class Solution(object):
-    def maxArea(self, height):
+    def threeSum(self, nums):
         """
-        :type height: List[int]
-        :rtype: int
+        :type nums: List[int]
+        :rtype: List[List[int]]
         """
-        # Base case
-        if len(height) == 2:
-            return min(height[0], height[1]) * 1
 
-        # Pointers
-        left = 0
-        right = len(height) - 1
+        # Start by sorting your list
+        nums.sort()
+        result = []
 
-        max_area = min(height[left], height[right]) * (right-left)
+        # We will use a fixed point starting at the beginning of the list
+        # We skip the last two numbers because we will have already checked those as we loop through
+        for i in range(len(nums) - 2):
+            # If the selected element is the same as the previous element, we can ignore this iteration of the loop because we already checked all permutations
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-        while left != right:
-            selected_max = min(height[left], height[right]) * (right-left)
+            # Set two points - left (which is one element to the right of the selected element), and right which starts from the last element in the list
+            left, right = i + 1, len(nums) - 1
 
-            if (selected_max) > max_area:
-                max_area = selected_max
+            # Slide pointers
+            while left < right:
+                # Check if the value of the selected elements is equal to 0
+                total = nums[i] + nums[left] + nums[right]
 
-            if height[left] < height[right]:
-                left += 1
-            else:
-                right -= 1
+                if total == 0:
+                    # If the sum is 0, add it to your results lise
+                    result.append([nums[i], nums[left], nums[right]])
 
-        return max_area
+                    # Here it gets hinkey.  You're still looping through the list sliding pointers; so, you need to skip duplicates
+                    # Basically just check the next incremented element from the left, and next decremented element on the right and skip duplicates until you find an element that is different
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    # If they aren't duplicates, slide your pointers
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
