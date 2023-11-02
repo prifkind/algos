01f8133198bb071d9837c7e347e53f035f28f5a0
Peter Rifkind <prifkind@gmail.com>
2023-11-02 14:43:32 -0400

    Daily algo

--- algo.py
+++ algo.py
@@ -1,25 +1,31 @@
 class Solution(object):
-    def isValid(self, s):
+    def search(self, nums, target):
         """
-        :type s: str
-        :rtype: bool
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
         """
+        # Set up pointers for left, right, and middle
+        left, right = 0, len(nums) - 1
 
-        # Maps for the brackets
-        bracket_map = {')': '(', '}': '{', ']': '['}
-        open_brackets = set(['(', '{', '['])
+        while left <= right:
+            mid = (left + right) // 2
 
-        stack = []
+            # If the middle element matches the target, return its index
+            if nums[mid] == target:
+                return mid
 
-        for char in s:
-            # If it's an opening bracket, push onto the stack
-            if char in open_brackets:
-                stack.append(char)
-            else:
-                # If the stack is empty or the top of the stack is not the corresponding opening bracket
-                if not stack or stack[-1] != bracket_map[char]:
-                    return False
-                stack.pop()
+            # Determine which half is sorted
+            if nums[left] <= nums[mid]:  # Left half is sorted
+                if nums[left] <= target <= nums[mid]:  # Check if target is within the left sorted half
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:  # Right half is sorted
+                if nums[mid] <= target <= nums[right]:  # Check if target is within the right sorted half
+                    left = mid + 1
+                else:
+                    right = mid - 1
 
-        # If the stack is empty, return True, otherwise return False
-        return len(stack) == 0
+        # If we've reached here, the target isn't in the list
+        return -1
