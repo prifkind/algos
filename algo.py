0bfb8135af70d9e8dddda766ebb7199b76660362
Peter Rifkind <prifkind@gmail.com>
2023-11-13 14:43:39 -0500

    Daily algo

--- algo.py
+++ algo.py
@@ -1,29 +1,8 @@
 class Solution(object):
-    def insert(self, intervals, newInterval):
+    def uniquePaths(self, m, n):
         """
-        :type intervals: List[List[int]]
-        :type newInterval: List[int]
-        :rtype: List[List[int]]
+        :type m: int
+        :type n: int
+        :rtype: int
         """
-        result = []
-        i = 0
-        start, end = newInterval
-
-        # Add all the intervals ending before newInterval starts
-        while i < len(intervals) and intervals[i][1] < start:
-            result.append(intervals[i])
-            i += 1
-
-        # Merge all overlapping intervals to one considering newInterval
-        while i < len(intervals) and intervals[i][0] <= end:
-            start = min(start, intervals[i][0])
-            end = max(end, intervals[i][1])
-            i += 1
-        result.append([start, end])  # add the merged interval
-
-        # Add all the rest
-        while i < len(intervals):
-            result.append(intervals[i])
-            i += 1
-
-        return result
+        return factorial(m+n-2) // (factorial(m-1) * factorial(n-1))
