51370682af3d5610a3010bb18ec588cc813f809f
Peter Rifkind <prifkind@gmail.com>
2023-11-27 12:59:17 -0500

    Daily algo 2

--- algo.py
+++ algo.py
@@ -1,31 +1,59 @@
+from collections import Counter
+
 class Solution:
-    def exist(self, board, word):
-        ROWS, COLS = len(board), len(board[0])
+    def minWindow(self, s, t):
+        if not t or not s:
+            return ""
 
-        def dfs(r, c, i):
-            ''' Depth-first search from board[r][c] for word[i:]. '''
-            if i == len(word):
-                return True
-            if (r < 0 or c < 0 or
-                r >= ROWS or c >= COLS or
-                word[i] != board[r][c] or
-                board[r][c] == "#"):  # Marked as part of the path
-                return False
+        # Dictionary which keeps a count of all the unique characters in t.
+        dict_t = Counter(t)
 
-            # Mark the choice before exploring further.
-            temp, board[r][c] = board[r][c], '#'
-            # Explore the four possible directions.
-            res = (dfs(r+1, c, i+1) or
-                   dfs(r-1, c, i+1) or
-                   dfs(r, c+1, i+1) or
-                   dfs(r, c-1, i+1))
-            # Unmark the choice, backtracking.
-            board[r][c] = temp
-            return res
+        # Number of unique characters in t, which need to be present in the desired window.
+        required = len(dict_t)
 
-        # Check every position on the board for the start of the word.
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r, c, 0):
-                    return True
-        return False
+        # Left and Right pointer
+        l, r = 0, 0
+
+        # formed is used to keep track of how many unique characters in t
+        # are present in the current window in its desired frequency.
+        # e.g. if t is "AABC" then the window must have two A's, one B and one C.
+        # Thus formed would be = 3 when all these conditions are met.
+        formed = 0
+
+        # Dictionary which keeps a count of all the unique characters in the current window.
+        window_counts = {}
+
+        # ans tuple of the form (window length, left, right)
+        ans = float("inf"), None, None
+
+        while r < len(s):
+            # Add one character from the right to the window
+            character = s[r]
+            window_counts[character] = window_counts.get(character, 0) + 1
+
+            # If the frequency of the current character added equals to the
+            # desired count in t then increment the formed count by 1.
+            if character in dict_t and window_counts[character] == dict_t[character]:
+                formed += 1
+
+            # Try and contract the window till the point where it ceases to be 'desirable'.
+            while l <= r and formed == required:
+                character = s[l]
+
+                # Save the smallest window until now.
+                if r - l + 1 < ans[0]:
+                    ans = (r - l + 1, l, r)
+
+                # The character at the position pointed by the
+                # `Left` pointer is no longer a part of the window.
+                window_counts[character] -= 1
+                if character in dict_t and window_counts[character] < dict_t[character]:
+                    formed -= 1
+
+                # Move the left pointer ahead, this would help to look for a new window.
+                l += 1
+
+            # Keep expanding the window once we are done contracting.
+            r += 1
+
+        return "" if ans[0] == float("inf") else s[ans[1]:ans[2] + 1]
